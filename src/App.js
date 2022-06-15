4f095a293caca838773b8ea05dc6615fba905c50
Tomic, Vladimir <Vladimir.Tomic@zuehlke.com>
2022-06-15 14:40:14 +0200

    Refactor API call

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ import "./App.css";
 import RecipeFilter from "./components/recipe-filter/RecipeFilter";
 
 function App() {
-  // Default page will be 1 whenever something changes (e.g. filter)
+  // Default page will be 1 whenever something changes (e.g. filter value)
   const [activePage, setActivePage] = useState(0);
+  // Total number of pages (for the Pagination widget)
   const [totalPages, setTotalPages] = useState(0);
-  // Initial list of recipes
+  // List of recipes to show
   const [recipes, setRecipes] = useState([]);
 
-  // Fetch all recipes from the API
-  const fetchRecipes = () =>
-    fetch(process.env.REACT_APP_BACKEND_URL)
+  // A function which will fetch recipes from the API, with or without search query (filter) or page number
+  const fetchRecipes = (search, page) => {
+    // Form the URL first, based on the presence of the two parameters
+    let URL = `${process.env.REACT_APP_BACKEND_URL}${
+      search !== null ? "?search=" + search : ""
+    }${page !== null ? "?page=" + page : ""}`;
+
+    // Call the API and set the recipes list, the active page number and the total number of pages
+    fetch(URL)
       .then((res) => res.json())
       .then((data) => {
         setRecipes(data.recipes);
         setActivePage(data.currentPage);
         setTotalPages(data.totalPages);
       });
+  };
 
+  // Do the real call when the page loads
   useEffect(() => {
-    fetchRecipes();
+    fetchRecipes(null, null);
   }, []);
 
+  // A function which will call the API when a user deletes a recipe
   const onRecipeDeleted = (recipe) => {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/${recipe.id}`, {
       method: "DELETE",
     })
-      .then((res) => fetchRecipes())
+      .then((res) => fetchRecipes(null, null))
       .catch((err) => console.log("Handle the exception if needed"));
   };
 
+  // When a user clicks a new page, call the API with the "page" parameter
   const onPageChange = (page) => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}?page=${page}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setRecipes(data.recipes);
-        setActivePage(data.currentPage);
-        setTotalPages(data.totalPages);
-      });
+    fetchRecipes(null, page);
   };
 
-  const onFilterChanged = (filterValue) => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}?search=${filterValue}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setRecipes(data.recipes);
-        setActivePage(data.currentPage);
-        setTotalPages(data.totalPages);
-      });
+  // When the filter value changes, call the API with the "search" parameter
+  const onFilterChanged = (search) => {
+    fetchRecipes(search, null);
   };
 
   return (
